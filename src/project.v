1379479fe768e83d0af3cda1058daeec8a583fa9
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-09-21 12:07:21 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_uabc_test2031 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,13 +15,34 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+    
+    reg [23:0] counter;          
+    reg [3:0]  display_value;    
+    wire [6:0] segment_reg;
+    assign uo_out[6:0] = segment_reg;
+    assign uo_out[7] = 1'b0;
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            counter <= 24'd0;
+            display_value <= 4'd0;
+        end else if (counter == 24'd10000000) begin
+            counter <= 24'd0;
+            display_value <= display_value + 1'b1;
+            
+            if (display_value == 4'd15) begin
+                display_value <= 4'd0;
+            end
+        end else begin
+            counter <= counter + 1'b1;
+        end
+    end
+
+    seg7 seg7(.digit(display_value), .segments(segment_reg));
+    
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+    wire _unused = &{ena,ui_in,uio_in};
 endmodule
