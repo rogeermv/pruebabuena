e39556289fedbafe51f143e03ac7242d18478093
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-09-21 12:10:34 -0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_uabc_test2032 user_project (
+  tt_um_uabc_testhoy user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
